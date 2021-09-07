b9cdb301d6697d39c3bdde76276ca193480c769d
surong <surong@968666.net>
2021-09-07 15:21:52 +0800

    登录接口搭建

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 const Koa=require('koa');
 const userRouter =require('../router/user.router');
+const authRouter =require('../router/auth.router');
 const bodyParser=require('koa-bodyparser');
 const errorHandle=require('./error.handle');
 
@@ -8,6 +9,9 @@ const app = new Koa();
 app.use(bodyParser());
 app.use(userRouter.routes());
 app.use(userRouter.allowedMethods());
+
+app.use(authRouter.routes());
+app.use(authRouter.allowedMethods());
 
 app.on('error',errorHandle);
 
