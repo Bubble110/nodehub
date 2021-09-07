a9fc38efabbab992778d68217cb0198abc48e0a8
surong <surong@968666.net>
2021-09-07 14:09:18 +0800

    注册接口

---
+++ src/app/error.handle.js
@@ -0,0 +1,23 @@
+const errorType=require('../constants/error-types');
+
+const errorHandler=(error,ctx)=>{
+  let status,message;
+
+  switch(error.message){
+    case errorType.NAME_OR_PASSWORD_IS_REQUIRED:
+      status=400;
+      message="用户名或者密码不能为空";
+      break;
+    case errorType.USER_ALREADY_EXISTS:
+      status=409;
+      message="用户已经存在";
+      break;
+    default:
+      status=404;
+      message="NOT FOUND";
+  }
+  ctx.status=status;
+  ctx.body=message
+}
+
+module.exports=errorHandler;
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,14 @@
 const Koa=require('koa');
 const userRouter =require('../router/user.router');
 const bodyParser=require('koa-bodyparser');
+const errorHandle=require('./error.handle');
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(userRouter.routes());
 app.use(userRouter.allowedMethods());
+
+app.on('error',errorHandle);
 
 module.exports=app;
