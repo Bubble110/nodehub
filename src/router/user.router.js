a1f100f58680514be81349f00b904f2aeb30e32a
surong <surong@968666.net>
2021-09-07 14:39:05 +0800

    密码加密存储

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -3,11 +3,12 @@ const {
   create
 } = require('../controller/user.controller')
 const {
-  verifyUser
+  verifyUser,
+  handlePassword
 }=require('../middleware/user.middleware')
 
 const userRouter=new Router({prefix:'/users'});
 
-userRouter.post('/',verifyUser,create)
+userRouter.post('/',verifyUser,handlePassword,create)
 
 module.exports=userRouter;
